174380086f69144a9990aa0f500de485461a35a4
jerinjoseph121 <jerinjosephch@gmail.com>
2021-04-21 15:22:31 +0400

    Implemented the recommendation system python script

--- src/pythonScripts/main.py
+++ src/pythonScripts/main.py
@@ -1,22 +1,58 @@
 import json
 import sys
+import os
+import pandas as pd
+
 args = sys.argv[1:]  # sys.argv[0] is the directory of this python script
 
-# just for testing the python-shell:
+# cwd is the current directory
+cwd = os.path.dirname(os.path.realpath(sys.argv[0]))
 
-# function to echo back the arguments:
+# C:/Users/jerin/Desktop/Jerin/Coding/Projects/movie-recommender/src/pythonScripts
+similarityDf = pd.read_json(
+    cwd + "\\movielensDataset\\correlation_matrix.json")
 
 
-def echo_args(args):
-    #a = int(a)
-    #b = int(b)
-    #c = a+b
-    return_to_python_shell(args)
+def echo_args(df):
+    result = df.to_json(orient="records")
+    parsed = json.loads(result)
+    return_to_python_shell(parsed)
 
 
 def return_to_python_shell(a):
     print(json.dumps(a))
 
 
+# Function which extracts the correlation of a movie from the similarity matrix and returns it
+def getSimilarMovies(movieName):
+    # This is used to make the correlation value ranging from 0-100
+    similar_score = (similarityDf[movieName] + 1) * 50
+    similar_score = similar_score.sort_values(ascending=False)
+    return similar_score
+
+
+# Here selection list is the user input for preferred movies
+selection = args
+
+input_length = len(selection)
+
+movie_similarity = pd.DataFrame()
+
+# Iterating through every movie in the user list and running the function to obtain its correlation array
+for movie in selection:
+    movie_dict = json.loads(movie)
+    movie_similarity = movie_similarity.append(
+        getSimilarMovies(movie_dict['title']), ignore_index=False)
+
+# Adding the values of the correlation array for every movie and sorting in descending order to obtain the final result
+movie_similarity = movie_similarity.sum().sort_values(ascending=False)
+
+# Obtaining the average value after the summation
+movie_similarity = movie_similarity / input_length
+
+movie_similarity = movie_similarity.to_frame().reset_index().round(2)
+movie_similarity = movie_similarity.rename(
+    columns={'index': 'title', 0: 'correlation'})
+
 # Code to execute:
-echo_args(args)
+echo_args(movie_similarity.head(10))
